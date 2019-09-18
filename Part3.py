f214bbd26035ada8668aa4fe06c52e6afad5d6a7
unknown <mhamza.1410@gmail.com>
2019-09-19 00:10:10 +0500

    message

---
+++ Part3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 00:05:50 2019
+
+@author: M.Hamza Ashraf
+"""
+
+from nltk.stem import SnowballStemmer
+import sys
+
+arg=sys.argv[2]
+
+print(arg)
